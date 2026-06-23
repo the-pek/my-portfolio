85c3a1fa3e6ff71f930d74ecbc267fd686fa8d6d
the-pek <pierrekengne2005@gmail.com>
2026-06-23 20:27:55 +0200

    Déplacement du contenu dans les bons fichiers

--- src/app/Pages/about/about.ts
+++ src/app/Pages/about/about.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
-  cv: String = 'documents/CV_Kengne_Pierre-Edwin.pdf';
 }
